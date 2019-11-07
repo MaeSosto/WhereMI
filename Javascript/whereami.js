fb35bc120ca6609af350a5ff3dd44afd3ec20755
andriy97 <50208588+andriy97@users.noreply.github.com>
2019-11-07 15:45:25 +0100

    finestra audio +  editor

--- Javascript/whereami.js
+++ Javascript/whereami.js
@@ -1,19 +1,12 @@
 
-      // This example adds a search box to a map, using the Google Place Autocomplete
-      // feature. People can enter geographical searches. The search box will return a
-      // pick list containing a mix of places and predicted search terms.
-
-      // This example requires the Places library. Include the libraries=places
-      // parameter when you first load the API. For example:
-      // <script src="https://maps.googleapis.com/maps/api/js?key=YOUR_API_KEY&libraries=places">
-
-      if(navigator.geolocation){ //controllo se è presente la geolocalizzazione
+     /**************SETTAGGIO MAPPA*************/
+     
+     if(navigator.geolocation){ //controllo se è presente la geolocalizzazione
           navigator.geolocation.getCurrentPosition(initAutocomplete);
       }else{
           console.log('position not available'); //messaggio di errore se non è presente
       }
       
-
 
       function initAutocomplete(position) {
         var coords = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
@@ -21,7 +14,8 @@
         var map = new google.maps.Map(document.getElementById('map'), {
           center: coords,
           zoom: 17,
-          mapTypeId: 'roadmap'
+          mapTypeId: 'roadmap',
+          disableDefaultUI: true
           
         });
         
@@ -226,65 +220,38 @@
    
 
 
- /******** AUDIO  ********/
+/**********FINESTRA PLAYER E AUDIO **********/
+function showPlayerDiv(show){   //mostra o nasconde la finestra del player e audio
+  var bar=document.getElementById('bar');
+  if (show==true) {
+    bar.style.visibility='visible';
+  }else{
+    bar.style.visibility='hidden';
+  }
 
- var player;
+}
+ 
+var urlvideo = ["FV-AmtffJpI", "1Z72j3W3eAc"]; //array con id dei video da riprodurre, creare funzione per popolarlo
+var player;
 
- function onYouTubeIframeAPIReady() {
 
- $("#playbutton").click(toggleAudio); //se clicchi sul pulsante chiama toogleAudio
 
- player = new YT.Player('youtube-player', { //lega il player al div "youtube-player"
-  height: '0',
-  width: '0',
-  videoId: "-Jqz3GQWhJY", //url del video (stringa a 11 caratteri, dopo youtube.com/watch?v=)
-  playerVars: {
-    autoplay: 0,
-    loop: 1,
-  },
+function onYouTubeIframeAPIReady() {
+
+$("#playbutton").click(toggleAudio); //se clicchi sul pulsante chiama toogleAudio
+
+player = new YT.Player('youtube-player', { //lega il player al div "youtube-player"
+ height: '0',
+ width: '0',
+ videoId: urlvideo[0], //url del video (stringa a 11 caratteri, dopo youtube.com/watch?v=)
+ playerVars: {
+   autoplay: 0,
+   loop: 0,
+ },
 });
 
 
 function toggleAudio() {
-  if ( player.getPlayerState() == 1 || player.getPlayerState() == 3 ) {
-    player.pauseVideo(); 
-  } else {
-    player.playVideo(); 
-  } 
-} 
- }
-
-
-
- /*
- var player;
-  function onYouTubeIframeAPIReady() {
-
-    var tubediv = document.getElementById("youtube-audio");
-    tubediv.innerHTML = '<img id="youtube-icon" src=""/><div id="youtube-player"></div>';
-    tubediv.style.cssText = 'width:150px;margin:2em auto;cursor:pointer;cursor:hand;display:none';
-    tubediv.onclick = toggleAudio;
-
-    player = new YT.Player('youtube-player', {
-      height: '0',
-      width: '0',
-      videoId: tubediv.dataset.video,
-      playerVars: {
-        autoplay: tubediv.dataset.autoplay,
-        loop: tubediv.dataset.loop,
-      },
-      events: {
-        'onReady': onPlayerReady,
-        'onStateChange': onPlayerStateChange 
-      } 
-    });
-  } 
-
-  function togglePlayButton(play) {    
-    document.getElementById("youtube-icon").src = play ? "https://i.imgur.com/IDzX9gL.png" : "https://i.imgur.com/quyUPXN.png";
-  }
-
-  function toggleAudio() {
     if ( player.getPlayerState() == 1 || player.getPlayerState() == 3 ) {
       player.pauseVideo(); 
       togglePlayButton(false);
@@ -293,18 +260,48 @@ function toggleAudio() {
       togglePlayButton(true);
     } 
   } 
+} 
 
-  function onPlayerReady(event) {
-    player.setPlaybackQuality("small");
-    document.getElementById("youtube-audio").style.display = "block";
-    togglePlayButton(player.getPlayerState() !== 5);
+
+
+function togglePlayButton(play) {    
+    document.getElementById("playbutton").innerHTML = play ? "pause" : "play";
   }
 
-  function onPlayerStateChange(event) {
-    if (event.data === 0) {
-      togglePlayButton(false); 
+  $("#video1button").click(function(){
+    player.loadVideoById(urlvideo[0]);
+    player.playVideo;
+    togglePlayButton(true);
+  }); 
+
+  $("#video2button").click(function(){
+    player.loadVideoById(urlvideo[1]);
+    player.playVideo;
+    togglePlayButton(true);
+  }); 
+
+  $("#nextbutton").click(function(){
+   var index = urlvideo.indexOf(player.getVideoData()['video_id']);  //prendo l'indice del video che è in esecuzione
+    if (index == urlvideo.length-1){ //se siamo sull'ultimo video
+      player.loadVideoById(urlvideo[0]);
     }
-  }
+    else{
+      player.loadVideoById(urlvideo[index+1]);
+    }
+    player.playVideo;
+    togglePlayButton(true);
+  }); 
+
+  $("#backbutton").click(function(){
+    var index = urlvideo.indexOf(player.getVideoData()['video_id']);
+    if (index-1<0){ //se siamo sul primo video
+      player.loadVideoById(urlvideo[0]);
+    }
+    else{
+      player.loadVideoById(urlvideo[index-1]);
+    }
+    player.playVideo;
+    togglePlayButton(true);
+  }); 
 
 
-*/
