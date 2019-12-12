d940b28c4c942fc92bcd8939ca233aee4a99f962
andriy97 <50208588+andriy97@users.noreply.github.com>
2019-12-12 21:15:18 +0100

    aggiunto div videosalvati + funzioni che ci stanno dietro

--- Javascript/editor.js
+++ Javascript/editor.js
@@ -1,10 +1,7 @@
-var audSave = document.getElementById('aud2');
+var audSave = document.getElementById('aud2'); //tag dove viene salvato l'audio registrato
 var titolo, scopo, lingua, categoria, descrizione, audience, dettagli;
-
-
-
-
-
+var player;
+var divMetadati = document.getElementById("metadatiupload");
 
 
 
@@ -19,19 +16,22 @@ slider.oninput = function () {
 
 
 function showAudio(show) { //mostra o nasconde il tag audio
-    var audio = document.getElementById('aud2');
+    
     if (show == true) {
         // audio.style.visibility = 'visible';
-        audio.style.display = 'block';
+        audSave.style.display = 'block';
+        divMetadati.style.display = "block";
     } else {
         // audio.style.visibility = 'hidden';
-        audio.style.display = 'none';
+        audSave.style.display = 'none';
+        divMetadati.style.display = "none";
     }
 
 }
 
 function toggleRegistra(registra) { // cambia tasto registra
     document.getElementById("btnStart").innerHTML = registra ? "Registra" : "Stop";
+
 }
 
 
@@ -79,7 +79,7 @@ navigator.mediaDevices.getUserMedia(constraintObj)
 
         //add listeners for saving audio/audio
         let start = document.getElementById('btnStart');
-        
+
         let mediaRecorder = new MediaRecorder(mediaStreamObj);
         let chunks = [];
         var started = false;
@@ -105,7 +105,7 @@ navigator.mediaDevices.getUserMedia(constraintObj)
             console.log(ev.data);
         }
         mediaRecorder.onstop = (ev) => {
-             let blob = new Blob(chunks, {
+            let blob = new Blob(chunks, {
                 'type': 'video/*;'
             }); //passo l'array all'oggetto blob che li salva nella variabile
             chunks = [];
@@ -137,7 +137,7 @@ recorder.addEventListener('change', function (e) {
 
 
 
-///////////////CARICAMENTO YOUTUBE////////////////
+///////////////INIT CLIENT E CARICAMENTO YOUTUBE////////////////
 
 
 
@@ -187,6 +187,7 @@ function initClient() {
     }, function (err) {
         console.error("Error loading GAPI client for API", err);
     });
+
 }
 
 /**
@@ -239,25 +240,46 @@ function handleSignoutClick(event) {
 }
 
 
+/////////////////UPLOAD VIDEO PUBBLICI///////////////////
+async function uploadYoutube() {
 
-window.uploadToYoutube = async function (urlClip, titolo, metadati, descrizioneClip) {
+    var openLocationCode = "da fare";
+    titolo = document.getElementById("titolo").value;
+    descrizione = document.getElementById("descrizione").value;
+    scopo = document.getElementById("scopo").value;
+    lingua = document.getElementById("lingua").value;
+    categoria = document.getElementById("categoria").value;
+    audience = document.getElementById("audience").value;
+    dettagli = document.getElementById("dettagli").value;
+
+
+    var metadatiClip = openLocationCode + ":" + titolo + ":" + descrizione + ":" + scopo + ":" + lingua + ":" + categoria + ":" + audience + ":" + dettagli;
+
+
+    var success = await window.uploadToYoutube(audSave.src || recorder.src, titolo, metadatiClip);
+    if (success) {
+        alert("caricato");
+    }
+}
+
+window.uploadToYoutube = async function (urlClip, titolo, metadati) {
     //Ottieni clip video da URL (hosted cloudinary.com)
     let response = await fetch(urlClip);
     var rawData = await response.blob();
     rawData.type = 'video/mp4';
     console.log("Preparo invio dati a Youtube (API)", rawData);
-    uploadRawFile(rawData, titolo, metadati, descrizioneClip);
+    uploadRawFile(rawData, titolo, metadati);
     return true;
 }
 
 
-async function uploadRawFile(videoclip, titolo, metadatiClip, descrizioneClip) {
+async function uploadRawFile(videoclip, titolo, metadatiClip) {
     var token = gapi.auth2.getAuthInstance().currentUser.get().getAuthResponse().access_token;
     var params = {
         snippet: {
             categoryId: 27,
             title: titolo,
-            description: metadatiClip + "%%%" + descrizioneClip,
+            description: metadatiClip,
             tags: [metadatiClip]
         },
         status: {
@@ -274,7 +296,89 @@ async function uploadRawFile(videoclip, titolo, metadatiClip, descrizioneClip) {
     request.append('video', metadatiYoutube);
     request.append('mediaBody', videoclip);
 
-    var okUpload;
+    //Upload via API youtube (POST)
+
+
+    $.ajax({
+            method: 'POST',
+            url: 'https://www.googleapis.com/upload/youtube/v3/videos?access_token=' + encodeURIComponent(token) +
+                '&part=snippet,status',
+            data: request,
+            cache: false,
+            contentType: false,
+            processData: false,
+        })
+        .done(function (response) {
+            console.log("Caricamento completato! YouTube:", response)
+            return true;
+        })
+        .fail(function (response) {
+            var errors = response.responseJSON.error.errors[0];
+            console.log("Errore API per Upload YT!", errors);
+            return false;
+        });
+
+
+
+}
+/////////////////UPLOAD VIDEO PRIVATI//////////////////////////////////////
+
+async function uploadYoutubePrivate() {
+
+    var openLocationCode = "da fare";
+    titolo = document.getElementById("titolo").value;
+    descrizione = document.getElementById("descrizione").value;
+    scopo = document.getElementById("scopo").value;
+    lingua = document.getElementById("lingua").value;
+    categoria = document.getElementById("categoria").value;
+    audience = document.getElementById("audience").value;
+    dettagli = document.getElementById("dettagli").value;
+
+
+    var metadatiClip = openLocationCode + ":" + titolo + ":" + descrizione + ":" + scopo + ":" + lingua + ":" + categoria + ":" + audience + ":" + dettagli;
+
+
+    var success = await window.uploadToYoutubePrivate(audSave.src || recorder.src, titolo, metadatiClip);
+    if (success) {
+        alert("caricato");
+    }
+}
+
+
+window.uploadToYoutubePrivate = async function (urlClip, titolo, metadati) {
+    //Ottieni clip video da URL (hosted cloudinary.com)
+    let response = await fetch(urlClip);
+    var rawData = await response.blob();
+    rawData.type = 'video/mp4';
+    console.log("Preparo invio dati a Youtube (API)", rawData);
+    uploadRawFilePrivate(rawData, titolo, metadati);
+    return true;
+}
+
+
+async function uploadRawFilePrivate(videoclip, titolo, metadatiClip) {
+    var token = gapi.auth2.getAuthInstance().currentUser.get().getAuthResponse().access_token;
+    var params = {
+        snippet: {
+            categoryId: 27,
+            title: titolo,
+            description: metadatiClip,
+            tags: [metadatiClip]
+        },
+        status: {
+            privacyStatus: 'unlisted',
+            embeddable: true
+        }
+    };
+
+    //Building request
+    var request = new FormData();
+    var metadatiYoutube = new Blob([JSON.stringify(params)], {
+        type: 'application/json'
+    });
+    request.append('video', metadatiYoutube);
+    request.append('mediaBody', videoclip);
+
     //Upload via API youtube (POST)
 
 
@@ -304,34 +408,158 @@ async function uploadRawFile(videoclip, titolo, metadatiClip, descrizioneClip) {
 
 
 
-$("#upload").click(uploadYoutube);
-
-
-
-async function uploadYoutube() {
+$("#upload").click(function(){
+    divMetadati.style.display= 'none';
+    uploadYoutube();
     
-var openLocationCode = "da fare";
-titolo = document.getElementById("titolo").value;
-descrizione = document.getElementById("descrizione").value;
-scopo = document.getElementById("scopo").value;
-lingua = document.getElementById("lingua").value;
-categoria = document.getElementById("categoria").value;
-audience = document.getElementById("audience").value;
-dettagli = document.getElementById("dettagli").value;
+});
+
+$("#salva").click(function(){
+    divMetadati.style.display= 'none';
+    uploadYoutubePrivate();
+});
 
 
-var metadatiClip = openLocationCode + ":" + titolo + ":" + descrizione + ":" + scopo + ":" + lingua + ":" + categoria + ":" + audience + ":" + dettagli;
+
+function getPlaylist(){ 
+    $("#videosalvatilist").html(''); //elimino contenuto lista prima di caricarla
+
+    var token = gapi.auth2.getAuthInstance().currentUser.get().getAuthResponse().access_token;
+    $.get(
+        "https://www.googleapis.com/youtube/v3/channels?access_token=" + encodeURIComponent(token), {
+            part: 'contentDetails',
+            mine: true,
+        },
+        function (data) {
+            $.each(data.items, function (i, item) {
+
+                var videos = item.contentDetails.relatedPlaylists.uploads;
+                getVids(videos);
+            })
+        }
+    )
+}
 
 
-    var success = await window.uploadToYoutube(audSave.src || recorder.src, titolo, metadatiClip, descrizioneClip);
-    if (success) {
-        alert("caricato");
+function getVids(videos) { //funzione che crea la lista di video salvati 
+    var token = gapi.auth2.getAuthInstance().currentUser.get().getAuthResponse().access_token;
+    $.get(
+        "https://www.googleapis.com/youtube/v3/playlistItems?access_token=" + encodeURIComponent(token), {
+            part: 'snippet, status',
+            maxResults: 20,
+            playlistId: videos
+        },
+        function (data) { 
+            $.each(data.items, function (i, item) {
+                if (item.status.privacyStatus == "unlisted") { //seleziono solo i video unlisted del canale
+
+                    output = '<li id="'+ item.snippet.resourceId.videoId+item.snippet.resourceId.videoId+'">' + item.snippet.title + '</li>' 
+                    + '<button type= "button"  id="' + item.snippet.resourceId.videoId+ item.snippet.resourceId.videoId+item.snippet.resourceId.videoId+ '">Play</button>'
+                    + '<button type= "button" id="' + item.snippet.resourceId.videoId + '">Carica</button>';
+                    $("#videosalvatilist").append(output); //aggiungo nomi e button alla lista dei video
+
+                    document.getElementById(item.snippet.resourceId.videoId+ item.snippet.resourceId.videoId+item.snippet.resourceId.videoId).onclick = function () { //riproduco il video salvato selezionato
+                        addToPlayer(item.snippet.resourceId.videoId);
+                    }
+                    document.getElementById(item.snippet.resourceId.videoId).onclick = function () {
+
+                        gapi.client.load("https://www.googleapis.com/discovery/v1/apis/youtube/v3/rest") //aggiungo all'oggetto client la possibilità di accedere ai video di youtube
+
+                        .then (function(){
+                            //aggiorno privacyStatus video youtube
+                            var request = gapi.client.youtube.videos.update({
+                                id: item.snippet.resourceId.videoId,
+                                part: 'status',
+                                status: {
+                                    privacyStatus: 'public'
+                                }
+                            });
+                        
+                            request.execute(function (response) {
+                                console.log(response);
+                            });
+                            // elimono dalla lista il video che è stato appena caricato e i suoi tasti
+                            var listId=item.snippet.resourceId.videoId+item.snippet.resourceId.videoId;
+                            var playId=item.snippet.resourceId.videoId+ item.snippet.resourceId.videoId+item.snippet.resourceId.videoId;
+                            var caricaId=item.snippet.resourceId.videoId;
+                            $("#"+listId).remove();
+                            $("#"+playId).remove();
+                            $("#"+caricaId).remove();
+                        })
+                    }
+                }
+            })
+
+        }
+    )
+}
+$("#prova").click(function () {
+
+getPlaylist();
+
+});
+
+
+////////PLAYER PER SENTIRE VIDEO SALVATI////////
+
+var tag = document.createElement('script');
+tag.src = "https://www.youtube.com/iframe_api";
+var firstScriptTag = document.getElementsByTagName('script')[0];
+firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+
+window.onYouTubeIframeAPIReady = function () {
+
+
+    player = new YT.Player('youtube-player2', { //lega il player al div "youtube-player2"
+        height: '0',
+        width: '0',
+        videoId: "eieeowLz-Ms", //url del video (stringa a 11 caratteri, dopo youtube.com/watch?v=)
+        playerVars: {
+            autoplay: 0,
+            loop: 0,
+        },
+    });
+
+
+}
+
+
+function addToPlayer(id) {
+    if (player) {
+        player.loadVideoById(id);
+        player.playVideo();
+        console.log(id);
+    } else {
+        console.log("variabile player non creata");
     }
 }
 
-//  RESPONSIVE HEADER
-$('ul.nav li.dropdown').hover(function () {
-    $(this).find('.dropdown-menu').stop(true, true).delay(200).fadeIn(500);
-}, function () {
-    $(this).find('.dropdown-menu').stop(true, true).delay(200).fadeOut(500);
+
+/*
+
+$("#prova2").click(function () {
+
+
+    gapi.client.load("https://www.googleapis.com/discovery/v1/apis/youtube/v3/rest") //aggiungo all'oggetto client la possibilità di accedere ai video di youtube
+    
+        .then (function(){
+            var request = gapi.client.youtube.videos.update({
+                id: "iOch785SV-c",
+                part: 'status',
+                status: {
+                    privacyStatus: 'private'
+                }
+            });
+        
+            request.execute(function (response) {
+                console.log(response);
+            });
+        })
+    
+
+  
+
+
 });
+
+*/
