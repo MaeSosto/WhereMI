a78c6323de51dfb88cf4695204a6d5f1d5fbcff2
Eriona Memko <>
2019-12-21 16:50:42 +0100

    created server with express and created new file structure (ps eseguire npm install to update libraries)

---
+++ media/Javascript/editor.js
@@ -0,0 +1,545 @@
+var audSave = document.getElementById('aud2'); //tag dove viene salvato l'audio registrato
+var titolo, scopo, lingua, categoria, descrizione, audience, dettagli;
+var player;
+var divMetadati = document.getElementById("metadatiupload");
+
+
+// Controllo sullo slider
+var slider = document.getElementById("dettagli");
+var output = document.getElementById("num_dettagli");
+output.innerHTML = slider.value;
+slider.oninput = function () {
+	output.innerHTML = this.value;
+}
+
+
+function showAudio(show) { //mostra o nasconde il tag audio
+
+	if (show == true) {
+		// audio.style.visibility = 'visible';
+		audSave.style.display = 'block';
+		divMetadati.style.display = "block";
+	} else {
+		// audio.style.visibility = 'hidden';
+		audSave.style.display = 'none';
+		divMetadati.style.display = "none";
+	}
+
+}
+
+function toggleRegistra(registra) { // cambia tasto registra
+	document.getElementById("btnStart").innerHTML = registra ? "Registra" : "Stop";
+
+}
+
+
+let constraintObj = {
+	audio: true,
+	video: true
+};
+
+if (navigator.mediaDevices === undefined) {
+	navigator.mediaDevices = {};
+	navigator.mediaDevices.getUserMedia = function (constraintObj) {
+		let getUserMedia = navigator.webkitGetUserMedia || navigator.mozGetUserMedia;
+		if (!getUserMedia) {
+			return Promise.reject(new Error('getUserMedia is not implemented in this browser'));
+		}
+		return new Promise(function (resolve, reject) {
+			getUserMedia.call(navigator, constraintObj, resolve, reject);
+		});
+	}
+} else {
+	navigator.mediaDevices.enumerateDevices()
+		.then(devices => {
+			devices.forEach(device => {
+				console.log(device.kind.toUpperCase(), device.label);
+				//, device.deviceId
+			})
+		})
+		.catch(err => {
+			console.log(err.name, err.message);
+		})
+}
+navigator.mediaDevices.getUserMedia(constraintObj)
+	.then(function (mediaStreamObj) {
+		//connect the media stream to the first audio element
+		let audio = document.querySelector('audio');
+		if ("srcObject" in audio) {
+			audio.srcObject = mediaStreamObj;
+		} else {
+			//old version
+			audio.src = window.URL.createObjectURL(mediaStreamObj);
+		}
+
+
+		//add listeners for saving audio/audio
+		let start = document.getElementById('btnStart');
+
+		let mediaRecorder = new MediaRecorder(mediaStreamObj);
+		let chunks = [];
+		var started = false;
+
+
+		start.addEventListener('click', (ev) => {
+			if (started == false) {
+				mediaRecorder.start();
+				started = true;
+				toggleRegistra(false);
+				showAudio(false);
+
+			} else {
+				mediaRecorder.stop();
+				started = false;
+				toggleRegistra(true);
+				showAudio(true);
+			}
+
+		})
+		mediaRecorder.ondataavailable = function (ev) {
+			chunks.push(ev.data); //salvo dati ricevuti in array
+			console.log(ev.data);
+		}
+		mediaRecorder.onstop = (ev) => {
+			let blob = new Blob(chunks, {
+				'type': 'video/*;'
+			}); //passo l'array all'oggetto blob che li salva nella variabile
+			chunks = [];
+			let audioURL = window.URL.createObjectURL(blob);
+			audSave.src = audioURL;
+		}
+	})
+
+
+///////////AUDIO DA PC////////
+
+const recorder = document.getElementById('recorder');
+
+
+recorder.addEventListener('change', function (e) {
+	const file = e.target.files[0];
+	const url = URL.createObjectURL(file);
+
+	// Do something with the audio file.
+	recorder.src = url;
+});
+
+
+///////////////INIT CLIENT E CARICAMENTO YOUTUBE////////////////
+
+const API_KEY = "AIzaSyAisQVJRCJqUAW-wICyJbshSxg_jPL-Y-A";
+const CLIENT_ID = "600073852662-qiaidgofjs1bt8dpd1jgm3tbk72sdlej.apps.googleusercontent.com";
+
+// Array of API discovery doc URLs for APIs used by the quickstart
+var DISCOVERY_DOCS = ["https://www.googleapis.com/discovery/v1/apis/people/v1/rest"];
+
+// Authorization scopes required by the API; multiple scopes can be
+// included, separated by spaces.
+var SCOPES = "https://www.googleapis.com/auth/youtube.upload https://www.googleapis.com/auth/youtube";
+
+var authorizeButton = document.getElementById('authorize_button');
+var signoutButton = document.getElementById('signout_button');
+var utenteButton = document.getElementById('buttonLogin');
+var update = document.getElementById('update');
+
+
+function handleClientLoad() {
+	gapi.load('client:auth2', initClient);
+}
+
+/**
+ *  Initializes the API client library and sets up sign-in state
+ *  listeners.
+ */
+function initClient() {
+	gapi.client.init({
+		apiKey: API_KEY,
+		clientId: CLIENT_ID,
+		discoveryDocs: DISCOVERY_DOCS,
+		scope: SCOPES
+	}).then(function () {
+		// Listen for sign-in state changes.
+		//alert("Sign-in successful");
+		gapi.auth2.getAuthInstance().isSignedIn.listen(updateSigninStatus);
+
+		// Handle the initial sign-in state.
+		updateSigninStatus(gapi.auth2.getAuthInstance().isSignedIn.get());
+		authorizeButton.onclick = handleAuthClick;
+		signoutButton.onclick = handleSignoutClick;
+		if (utenteButton) {
+			utenteButton.onclick = handleAuthClick;
+		}
+	}, function (err) {
+		console.error("Error loading GAPI client for API", err);
+	});
+
+}
+
+/**
+ *  Called when the signed in status changes, to update the UI
+ *  appropriately. After a sign-in, the API is called.
+ */
+function updateSigninStatus(isSignedIn) {
+	if (isSignedIn) {
+		authorizeButton.style.display = 'none';
+		signoutButton.style.display = 'block';
+
+		//Elementi statici presenti solo nel editor
+		if (utenteButton || update) {
+			utenteButton.style.display = 'none';
+			update.disabled = false;
+		}
+		//document.getElementById("update").display = 'initial';
+		//document.getElementById("update").disabled = false;
+	} else {
+		authorizeButton.style.display = 'block';
+		signoutButton.style.display = 'none';
+		if (utenteButton || update) {
+			utenteButton.style.display = 'none';
+			update.disabled = true;
+		}
+
+	}
+}
+
+/**
+ *  Sign in the user upon button click.
+ */
+function handleAuthClick(event) {
+	if (gapi.auth2.getAuthInstance().isSignedIn.get()) {
+		alert("Sei già loggato");
+		if (utenteButton || update) {
+			utenteButton.style.display = 'none';
+			update.disabled = false;
+		}
+	} else {
+		gapi.auth2.getAuthInstance().signIn();
+	}
+}
+
+/**
+ *  Sign out the user upon button click.
+ */
+function handleSignoutClick(event) {
+	gapi.auth2.getAuthInstance().signOut();
+}
+
+
+/////////////////UPLOAD VIDEO PUBBLICI///////////////////
+async function uploadYoutube() {
+
+	var openLocationCode = "da fare";
+	titolo = document.getElementById("titolo").value;
+	descrizione = document.getElementById("descrizione").value;
+	scopo = document.getElementById("scopo").value;
+	lingua = document.getElementById("lingua").value;
+	categoria = document.getElementById("categoria").value;
+	audience = document.getElementById("audience").value;
+	dettagli = document.getElementById("dettagli").value;
+
+
+	var metadatiClip = openLocationCode + ":" + titolo + ":" + descrizione + ":" + scopo + ":" + lingua + ":" + categoria + ":" + audience + ":" + dettagli;
+
+
+	var success = await window.uploadToYoutube(audSave.src || recorder.src, titolo, metadatiClip);
+	if (success) {
+		alert("caricato");
+	}
+}
+
+window.uploadToYoutube = async function (urlClip, titolo, metadati) {
+	//Ottieni clip video da URL (hosted cloudinary.com)
+	let response = await fetch(urlClip);
+	var rawData = await response.blob();
+	rawData.type = 'video/mp4';
+	console.log("Preparo invio dati a Youtube (API)", rawData);
+	uploadRawFile(rawData, titolo, metadati);
+	return true;
+}
+
+
+async function uploadRawFile(videoclip, titolo, metadatiClip) {
+	var token = gapi.auth2.getAuthInstance().currentUser.get().getAuthResponse().access_token;
+	var params = {
+		snippet: {
+			categoryId: 27,
+			title: titolo,
+			description: metadatiClip,
+			tags: [metadatiClip]
+		},
+		status: {
+			privacyStatus: 'public',
+			embeddable: true
+		}
+	};
+
+	//Building request
+	var request = new FormData();
+	var metadatiYoutube = new Blob([JSON.stringify(params)], {
+		type: 'application/json'
+	});
+	request.append('video', metadatiYoutube);
+	request.append('mediaBody', videoclip);
+
+	//Upload via API youtube (POST)
+
+
+	$.ajax({
+			method: 'POST',
+			url: 'https://www.googleapis.com/upload/youtube/v3/videos?access_token=' + encodeURIComponent(token) +
+				'&part=snippet,status',
+			data: request,
+			cache: false,
+			contentType: false,
+			processData: false,
+		})
+		.done(function (response) {
+			console.log("Caricamento completato! YouTube:", response)
+			return true;
+		})
+		.fail(function (response) {
+			var errors = response.responseJSON.error.errors[0];
+			console.log("Errore API per Upload YT!", errors);
+			return false;
+		});
+
+
+}
+/////////////////UPLOAD VIDEO PRIVATI//////////////////////////////////////
+
+async function uploadYoutubePrivate() {
+
+	var openLocationCode = "da fare";
+	titolo = document.getElementById("titolo").value;
+	descrizione = document.getElementById("descrizione").value;
+	scopo = document.getElementById("scopo").value;
+	lingua = document.getElementById("lingua").value;
+	categoria = document.getElementById("categoria").value;
+	audience = document.getElementById("audience").value;
+	dettagli = document.getElementById("dettagli").value;
+
+
+	var metadatiClip = openLocationCode + ":" + titolo + ":" + descrizione + ":" + scopo + ":" + lingua + ":" + categoria + ":" + audience + ":" + dettagli;
+
+
+	var success = await window.uploadToYoutubePrivate(audSave.src || recorder.src, titolo, metadatiClip);
+	if (success) {
+		alert("caricato");
+	}
+}
+
+
+window.uploadToYoutubePrivate = async function (urlClip, titolo, metadati) {
+	//Ottieni clip video da URL (hosted cloudinary.com)
+	let response = await fetch(urlClip);
+	var rawData = await response.blob();
+	rawData.type = 'video/mp4';
+	console.log("Preparo invio dati a Youtube (API)", rawData);
+	uploadRawFilePrivate(rawData, titolo, metadati);
+	return true;
+}
+
+
+async function uploadRawFilePrivate(videoclip, titolo, metadatiClip) {
+	var token = gapi.auth2.getAuthInstance().currentUser.get().getAuthResponse().access_token;
+	var params = {
+		snippet: {
+			categoryId: 27,
+			title: titolo,
+			description: metadatiClip,
+			tags: [metadatiClip]
+		},
+		status: {
+			privacyStatus: 'unlisted',
+			embeddable: true
+		}
+	};
+
+	//Building request
+	var request = new FormData();
+	var metadatiYoutube = new Blob([JSON.stringify(params)], {
+		type: 'application/json'
+	});
+	request.append('video', metadatiYoutube);
+	request.append('mediaBody', videoclip);
+
+	//Upload via API youtube (POST)
+
+
+	$.ajax({
+			method: 'POST',
+			url: 'https://www.googleapis.com/upload/youtube/v3/videos?access_token=' + encodeURIComponent(token) +
+				'&part=snippet,status',
+			data: request,
+			cache: false,
+			contentType: false,
+			processData: false,
+		})
+		.done(function (response) {
+			console.log("Caricamento completato! YouTube:", response)
+			return true;
+		})
+		.fail(function (response) {
+			var errors = response.responseJSON.error.errors[0];
+			console.log("Errore API per Upload YT!", errors);
+			return false;
+		});
+
+
+}
+
+
+$("#upload").click(function () {
+	divMetadati.style.display = 'none';
+	uploadYoutube();
+
+});
+
+$("#salva").click(function () {
+	divMetadati.style.display = 'none';
+	uploadYoutubePrivate();
+});
+
+
+function getPlaylist() {
+	$("#videosalvatilist").html(''); //elimino contenuto lista prima di caricarla
+
+	var token = gapi.auth2.getAuthInstance().currentUser.get().getAuthResponse().access_token;
+	$.get(
+		"https://www.googleapis.com/youtube/v3/channels?access_token=" + encodeURIComponent(token), {
+			part: 'contentDetails',
+			mine: true,
+		},
+		function (data) {
+			$.each(data.items, function (i, item) {
+
+				var videos = item.contentDetails.relatedPlaylists.uploads;
+				getVids(videos);
+			})
+		}
+	)
+}
+
+
+function getVids(videos) { //funzione che crea la lista di video salvati 
+	var token = gapi.auth2.getAuthInstance().currentUser.get().getAuthResponse().access_token;
+	$.get(
+		"https://www.googleapis.com/youtube/v3/playlistItems?access_token=" + encodeURIComponent(token), {
+			part: 'snippet, status',
+			maxResults: 20,
+			playlistId: videos
+		},
+		function (data) {
+			$.each(data.items, function (i, item) {
+				if (item.status.privacyStatus == "unlisted") { //seleziono solo i video unlisted del canale
+
+					output = '<li id="' + item.snippet.resourceId.videoId + item.snippet.resourceId.videoId + '">' + item.snippet.title + '</li>' +
+						'<button type= "button"  id="' + item.snippet.resourceId.videoId + item.snippet.resourceId.videoId + item.snippet.resourceId.videoId + '">Play</button>' +
+						'<button type= "button" id="' + item.snippet.resourceId.videoId + '">Carica</button>';
+					$("#videosalvatilist").append(output); //aggiungo nomi e button alla lista dei video
+
+					document.getElementById(item.snippet.resourceId.videoId + item.snippet.resourceId.videoId + item.snippet.resourceId.videoId).onclick = function () { //riproduco il video salvato selezionato
+						addToPlayer(item.snippet.resourceId.videoId);
+					}
+					document.getElementById(item.snippet.resourceId.videoId).onclick = function () {
+
+						gapi.client.load("https://www.googleapis.com/discovery/v1/apis/youtube/v3/rest") //aggiungo all'oggetto client la possibilità di accedere ai video di youtube
+
+							.then(function () {
+								//aggiorno privacyStatus video youtube
+								var request = gapi.client.youtube.videos.update({
+									id: item.snippet.resourceId.videoId,
+									part: 'status',
+									status: {
+										privacyStatus: 'public'
+									}
+								});
+
+								request.execute(function (response) {
+									console.log(response);
+								});
+								// elimono dalla lista il video che è stato appena caricato e i suoi tasti
+								var listId = item.snippet.resourceId.videoId + item.snippet.resourceId.videoId;
+								var playId = item.snippet.resourceId.videoId + item.snippet.resourceId.videoId + item.snippet.resourceId.videoId;
+								var caricaId = item.snippet.resourceId.videoId;
+								$("#" + listId).remove();
+								$("#" + playId).remove();
+								$("#" + caricaId).remove();
+							})
+					}
+				}
+			})
+
+		}
+	)
+}
+$("#prova").click(function () {
+
+	getPlaylist();
+
+});
+
+
+////////PLAYER PER SENTIRE VIDEO SALVATI////////
+
+var tag = document.createElement('script');
+tag.src = "https://www.youtube.com/iframe_api";
+var firstScriptTag = document.getElementsByTagName('script')[0];
+firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+
+window.onYouTubeIframeAPIReady = function () {
+
+
+	player = new YT.Player('youtube-player2', { //lega il player al div "youtube-player2"
+		height: '0',
+		width: '0',
+		videoId: "eieeowLz-Ms", //url del video (stringa a 11 caratteri, dopo youtube.com/watch?v=)
+		playerVars: {
+			autoplay: 0,
+			loop: 0,
+		},
+	});
+
+
+}
+
+
+function addToPlayer(id) {
+	if (player) {
+		player.loadVideoById(id);
+		player.playVideo();
+		console.log(id);
+	} else {
+		console.log("variabile player non creata");
+	}
+}
+
+
+/*
+
+$("#prova2").click(function () {
+
+
+    gapi.client.load("https://www.googleapis.com/discovery/v1/apis/youtube/v3/rest") //aggiungo all'oggetto client la possibilità di accedere ai video di youtube
+    
+        .then (function(){
+            var request = gapi.client.youtube.videos.update({
+                id: "iOch785SV-c",
+                part: 'status',
+                status: {
+                    privacyStatus: 'private'
+                }
+            });
+        
+            request.execute(function (response) {
+                console.log(response);
+            });
+        })
+    
+
+  
+
+
+});
+
+*/
---
+++ media/Javascript/whereami.js
@@ -0,0 +1,437 @@
+//  if(navigator.geolocation){ //controllo se è presente la geolocalizzazione
+//       navigator.geolocation.getCurrentPosition(initAutocomplete);
+//   }else{
+//       console.log('position not available'); //messaggio di errore se non è presente
+//   }
+
+
+/**************SETTAGGIO MAPPA*************/
+
+// funzione pr l'inizializazione delle coordinate
+
+function initCoords() {
+	if (navigator.geolocation) {
+		navigator.geolocation.getCurrentPosition(initAutocomplete);
+	} else {
+		showError("Your browser does not support Geolocation!");
+	}
+}
+
+
+function initAutocomplete(position) {
+	var coords = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
+
+	var directionsRenderer = new google.maps.DirectionsRenderer;
+	var directionsService = new google.maps.DirectionsService;
+	var geocoder = new google.maps.Geocoder();
+	var map = creamappa(coords);
+
+	var marker = creaMarker(coords);
+
+	marker.setMap(map);
+
+	function geocodeAddress(geocoder, resultsMap) {
+		marker.setMap(null);
+		var address = document.getElementById('pos').value;
+		geocoder.geocode({
+			'address': address
+		}, function (results) {
+			resultsMap.setCenter(results[0].geometry.location);
+			marker.setPosition(results[0].geometry.location)
+		});
+		showBar(false);
+		return (marker)
+	}
+
+	function calculateAndDisplayRoute(directionsService, directionsRenderer) {
+		var end = document.getElementById('end').value;
+		directionsService.route({
+			origin: marker.position,
+			destination: end,
+			travelMode: 'WALKING',
+
+		}, function (response, status) {
+			if (status === 'OK') {
+				directionsRenderer.setDirections(response);
+			} else {
+				console.log(status);
+			}
+		});
+
+	}
+
+	directionsRenderer.setMap(map);
+	//directionsRenderer.setPanel(document.getElementById('right-panel'));
+
+
+	compiler(end, map);
+	compiler(pos, map);
+	compiler(newMarker, map);
+
+	document.getElementById('newMarker').addEventListener('change', function () {
+		StorageMarker(geocoder, map);
+	});
+
+	document.getElementById('end').addEventListener('change', function () {
+		calculateAndDisplayRoute(directionsService, directionsRenderer, marker)
+	});
+
+	document.getElementById('pos').addEventListener('change', function () {
+		marker = geocodeAddress(geocoder, map, marker);
+		marker.setMap(map);
+	});
+
+	document.getElementById('reset-map').addEventListener('click', function () {
+		map.setCenter(marker.position);
+		map.setZoom(15)
+	});
+
+	google.maps.event.addListener(marker, 'dragend', function () {
+		marker.setPosition(marker.getPosition());
+
+	});
+
+}
+
+function compiler(input, map) {
+
+	var searchBox = new google.maps.places.SearchBox(input);
+	map.controls.push(input);
+
+	// Bias the SearchBox results towards current map's viewport.
+	map.addListener('bounds_changed', function () {
+		searchBox.setBounds(map.getBounds());
+	});
+
+	searchBox.addListener('places_changed', function () {
+		var places = searchBox.getPlaces();
+
+		if (places.length == 0) {
+			return;
+		}
+
+	})
+}
+
+function creamappa(coords) {
+	var map = new google.maps.Map(document.getElementById('map'), {
+		zoom: 15,
+		center: coords,
+	});
+	return (map);
+}
+
+function creaMarker(coords) {
+	var marker = new google.maps.Marker({
+		position: coords,
+		draggable: true,
+		animation: google.maps.Animation.DROP,
+		id: "marker"
+	});
+	return marker;
+}
+
+function stampaMarker(marker, map) {
+	marker.setMap(map);
+}
+
+
+function StorageMarker(geocoder, map) {
+	var nome = document.getElementById('newMarker').value;
+	var marker = addressconverter(geocoder, map, nome);
+	var titolo = document.getElementById("titolo").value;
+	var descrizione = document.getElementById("descrizione").value;
+	var scopo = document.getElementById("scopo").value;
+	var lingua = document.getElementById("lingua").value;
+	var categoria = document.getElementById("categoria").value;
+	var audience = document.getElementById("audience").value;
+	var dettagli = document.getElementById("dettagli").value;
+
+	Luogo(marker, lingua, audience, dettagli, descrizione, scopo, titolo, categoria)
+}
+
+function addressconverter(geocoder, resultsMap, address) {
+
+	var results = geocoder.geocode({
+		'address': address
+	});
+
+	function x(results) {
+		var marker;
+		resultsMap.setCenter(results[0].geometry.location);
+		marker = creaMarker(results[0].geometry.location)
+		return marker
+	}
+
+	return x(results);
+
+}
+
+
+/*********** LOGIN BUTTON ***********/
+
+function onSignIn(googleUser) {
+	// Useful data for your client-side scripts:
+	var profile = googleUser.getBasicProfile();
+	console.log("ID: " + profile.getId()); // Don't send this directly to your server!
+	console.log('Full Name: ' + profile.getName());
+	console.log('Given Name: ' + profile.getGivenName());
+	console.log('Family Name: ' + profile.getFamilyName());
+	console.log("Image URL: " + profile.getImageUrl());
+	console.log("Email: " + profile.getEmail());
+
+	// The ID token you need to pass to your backend:
+	var id_token = googleUser.getAuthResponse().id_token;
+	console.log("ID Token: " + id_token);
+}
+
+
+/**********FINESTRA PLAYER E AUDIO **********/
+
+function showPlayerDiv(show) { //mostra o nasconde la finestra del player e audio
+	var bar = document.getElementById('bar');
+	if (show == true) {
+		bar.style.visibility = 'visible';
+	} else {
+		bar.style.visibility = 'hidden';
+	}
+
+}
+
+function showBar(show) { //mostra o nasconde la finestra del player e audio
+
+	var bottone = document.getElementById('set-position');
+	var barra = document.getElementById('pos');
+	if (show == true) {
+		bottone.style.visibility = 'hidden';
+		barra.style.visibility = 'visible';
+	} else {
+		bottone.style.visibility = 'visible';
+		barra.style.visibility = 'hidden';
+	}
+
+}
+
+
+var urlvideo = ["FV-AmtffJpI", "1Z72j3W3eAc"]; //array con id dei video da riprodurre, creare funzione per popolarlo
+var player;
+
+
+function onYouTubeIframeAPIReady() {
+
+	$("#playbutton").click(toggleAudio); //se clicchi sul pulsante chiama toogleAudio
+
+	player = new YT.Player('youtube-player', { //lega il player al div "youtube-player"
+		height: '0',
+		width: '0',
+		videoId: urlvideo[0], //url del video (stringa a 11 caratteri, dopo youtube.com/watch?v=)
+		playerVars: {
+			autoplay: 0,
+			loop: 0,
+		},
+	});
+
+
+	function toggleAudio() {
+		if (player.getPlayerState() == 1 || player.getPlayerState() == 3) {
+			player.pauseVideo();
+			togglePlayButton(false);
+		} else {
+			player.playVideo();
+			togglePlayButton(true);
+		}
+	}
+
+	function togglePlayButton(play) {
+		document.getElementById("playbutton").innerHTML = play ? "pause" : "play";
+	}
+
+
+	$("#video1button").click(function () {
+		player.loadVideoById(urlvideo[0]);
+		player.playVideo;
+		togglePlayButton(true);
+	});
+
+	$("#video2button").click(function () {
+		player.loadVideoById(urlvideo[1]);
+		player.playVideo;
+		togglePlayButton(true);
+	});
+
+	$("#nextbutton").click(function () {
+		var index = urlvideo.indexOf(player.getVideoData()['video_id']); //prendo l'indice del video che è in esecuzione
+		if (index == urlvideo.length - 1) { //se siamo sull'ultimo video
+			player.loadVideoById(urlvideo[0]);
+		} else {
+			player.loadVideoById(urlvideo[index + 1]);
+		}
+		player.playVideo;
+		togglePlayButton(true);
+	});
+
+	$("#backbutton").click(function () {
+		var index = urlvideo.indexOf(player.getVideoData()['video_id']);
+		if (index - 1 < 0) { //se siamo sul primo video
+			player.loadVideoById(urlvideo[0]);
+		} else {
+			player.loadVideoById(urlvideo[index - 1]);
+		}
+		player.playVideo;
+		togglePlayButton(true);
+	});
+}
+
+class Luogo {
+
+	constructor(marker, lingua, audience, dettagli, descrizione, scopo, titolo, categoria) {
+		this.marker = Marker
+		this.lingua = lingua
+		this.audience = audience
+		this.dettagli = dettagli
+		this.descrizione = descrizione
+		this.scopo = scopo
+		this.titolo = titolo
+		this.categoria = categoria
+
+	}
+
+	getaudience() {
+		return this.audience;
+	}
+	getlingua() {
+		return this.lingua
+	}
+	getdettagli() {
+		return this.dettagli;
+	}
+	getmarker() {
+		return this.marker;
+	}
+	getdescrizione() {
+		return this.descrizione;
+	}
+	getscopo() {
+		return this.scopo
+	}
+	gettitolo() {
+		return this.titolo
+	}
+	getcategoria() {
+		return this.categoria
+	}
+}
+
+
+/*************** FILTRO e creazione Luoghi **************/
+/*
+ //var marker=creaMarker(coords);
+ var arra=new Array();
+ var c;
+ //var nome= document.getElementById('newMarker').value;
+ //var marker=addressconverter(geocoder,map,nome);
+ function b(){
+     var titolo = document.getElementById("titolo").value;
+     var descrizione = document.getElementById("descrizione").value;
+     var scopo = document.getElementById("scopo").value;
+     var lingua = document.getElementById("lingua").value;
+     var categoria = document.getElementById("categoria").value;
+     var audience = document.getElementById("audience").value;
+     var dettagli = document.getElementById("dettagli").value;
+     return (c=new Luogo(2,lingua,audience,dettagli,descrizione,scopo,titolo,categoria))
+ }
+ document.getElementById('clicca').addEventListener('click', function(){
+         x=b();
+         arra.push(x)
+         console.log(arra[0].getmarker()+arra[0].gettitolo()+arra[0].getdescrizione()+arra[0].getlingua())
+     }
+ );
+ function addressconverter(geocoder,resultsMap,address) {
+     var results=geocoder.geocode({'address': address});
+     function x(results) {
+         var marker;
+         resultsMap.setCenter(results[0].geometry.location);
+         marker=creaMarker(results[0].geometry.location)
+         return marker
+     }
+     return x(results);
+ }
+ function filter(input){
+     var y=arra.length;
+     var controllo=0
+     var foo=new Array()
+     for (var i=0;i<y;i++){
+         controllo=0
+         if(arra[i].getlingua()==input.getlingua()&&controllo==0){
+             foo.push(arra[i])
+             controllo++;
+         }
+         if(arra[i].getscopo()==input.getscopo()&&controllo==0){
+             foo.push(arra[i])
+             controllo++;
+         }
+         if(arra[i].getaudience()==input.getaudience()&&controllo==0){
+             foo.push(arra[i])
+             controllo++;
+         }
+         if(arra[i].getdettagli()==input.getdettagli()&&controllo==0){
+             foo.push(arra[i])
+             controllo++;
+         }
+         if(arra[i].getcategoria()==input.getcategoria()&&controllo==0){
+             foo.push(arra[i])
+             controllo++;
+         }
+     }
+     return foo;
+ }
+ function displayfilter(input,map) {
+     var y = input.length;
+     for (var i = 0; i < y; i++) {
+         stampaMarker(input[i].getmarker().map);
+     }
+ }
+ function resetFilter(input){
+     var y = input.length;
+     for (var i = 0; i < y; i++) {
+         input[i].getmarker().setMap(null);
+     }
+     input=[];
+ }
+ class Luogo{
+     constructor(marker,lingua,audience,dettagli,descrizione,scopo,titolo,categoria) {
+         this.marker = marker
+         this.lingua = lingua
+         this.audience = audience
+         this.dettagli = dettagli
+         this.descrizione = descrizione
+         this.scopo=scopo
+         this.titolo=titolo
+         this.categoria=categoria
+     }
+     getaudience(){
+         return this.audience;
+     }
+     getlingua(){
+         return this.lingua
+     }
+     getdettagli(){
+         return this.dettagli;
+     }
+     getmarker (){
+         return this.marker
+     }
+     getdescrizione(){
+         return this.descrizione;
+     }
+     getscopo(){
+         return this.scopo
+     }
+     gettitolo(){
+         return this.titolo
+     }
+     getcategoria(){
+         return this.categoria
+     }
+ }
+ */
